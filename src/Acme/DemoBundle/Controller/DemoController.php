6609e38b8d7b151c103429969e274c9fdc24ad2c
waleed mazhar <waleed731@live.com>
2015-04-30 17:54:32 +0500

    added tutorial

--- src/Acme/DemoBundle/Controller/DemoController.php
+++ src/Acme/DemoBundle/Controller/DemoController.php
@@ -13,6 +13,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DemoController extends Controller
 {
+
+	// BY WALEED: BELOW COMMITED LINES ARE IMPORTANT THIS IS CALLED ANNOTATION @Route WILL DEFINE WHAT WILL BE THE URL
+	// OF BELOW ACTION. THE METHOD indexAction IS CALLED ACTION IN SYMFONY
+	// EVERY PUBLIC METHOD IN CONTROLLER CLASS IS CORRESPOND TO A URL.
+
+	// SO WHEN YOU CONFIGURE SYMFONY ON YOUR LOCAL HOST AND TYPE HTTP://LOCALHOST  BELOW INDEX ACTION WILL RUN BECAUSE IT HAS
+	// "/" ROUTE IF YOU PUT @Route("/noel-is-tesing", name="_demo") THEN YOU WILL TYPE HTTP://LOCALHOST/neol-is-testing to run
+	//beLow index method. ROUTE NAME MUST BE UNIQUE
+
+	// @template() ANNOTATION WILL DEFINE WHAT VIEW WILL BE USED FOR THIS ACTION. EMPTY MEAN IT WILL SEARCH SAME NAME "INDEX" IN
+	// src/Acme/Demobundle/Resources/views/Demo/index.html.twig
+
+	// if you put @Template('contact') THEN SYMFONY WILL "CONTACT" VIEW INTO INDEX ACTION
+
     /**
      * @Route("/", name="_demo")
      * @Template()
